9d9b709461cdc86a1039d4bbb3e8e14f4e694803
Cade Clark <crazycademan@gmail.com>
2020-08-19 14:09:55 -0600

    Updates

---
+++ precache-manifest.2a9edc284b2244f8cc1e24df83c412f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd1927bbbafec4ffb23ed33f4ace245",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "6a4ca0efa5e2ea327b31",
+    "url": "/react-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cfb8fd0497ee10b1d00b",
+    "url": "/react-portfolio/static/js/2.39ced5cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.39ced5cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4ca0efa5e2ea327b31",
+    "url": "/react-portfolio/static/js/main.b40fae58.chunk.js"
+  },
+  {
+    "revision": "f68f9ee8eff3fd762197",
+    "url": "/react-portfolio/static/js/runtime-main.60bfa933.js"
+  },
+  {
+    "revision": "9cd16925b49e1a50bb05680e383f7714",
+    "url": "/react-portfolio/static/media/profile.9cd16925.jpg"
+  }
+]);
